2319efbf68fbb0503ccbe54574feffa4722ccb21
jostautas <jostautas.sakas@gmail.com>
2024-03-21 10:53:47 +0200

    two new columns at index.xhtml

--- src/main/java/lt/vu/usecases/Authors.java
+++ src/main/java/lt/vu/usecases/Authors.java
@@ -3,13 +3,16 @@ package lt.vu.usecases;
 import lombok.Getter;
 import lombok.Setter;
 import lt.vu.entities.Author;
+import lt.vu.entities.Book;
 import lt.vu.persistence.AuthorDAO;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Model
 public class Authors {
@@ -35,5 +38,29 @@ public class Authors {
 
     private void loadAllAuthors(){
         this.allAuthors = authorsDAO.loadAll();
+    }
+
+//    public String findCoAuthors(Author author) {
+//        StringBuilder coAuthors = new StringBuilder();
+//        for (Book book : author.getBooks()) {
+//            for (Author coAuthor : book.getAuthors()) {
+//                if (!coAuthor.equals(author)) {
+//                    if (coAuthors.length() > 0) coAuthors.append(", ");
+//                    coAuthors.append(coAuthor.getName());
+//                }
+//            }
+//        }
+//        return coAuthors.toString();
+//    }
+    public String findCoAuthors(Author author) {
+        Set<String> coAuthorNames = new HashSet<>();
+        for (Book book : author.getBooks()) {
+            for (Author coAuthor : book.getAuthors()) {
+                if (!coAuthor.equals(author)) {
+                    coAuthorNames.add(coAuthor.getName());
+                }
+            }
+        }
+        return String.join(", ", coAuthorNames);
     }
 }
