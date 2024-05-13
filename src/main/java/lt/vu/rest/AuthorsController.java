14d6bbd0f8481aa14cf7841c99a7959c2604e4cf
Jostautas <69266048+Jostautas@users.noreply.github.com>
2024-05-13 22:11:42 +0300

    GET method done

--- src/main/java/lt/vu/rest/AuthorsController.java
+++ src/main/java/lt/vu/rest/AuthorsController.java
@@ -8,9 +8,7 @@ import lt.vu.persistence.AuthorDAO;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -21,6 +19,17 @@ import javax.ws.rs.core.Response;
 public class AuthorsController {
     @Inject
     AuthorDAO authorDAO;
+
+    @Path("/{id}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+    public Response getById(@PathParam("id") final Integer id) {
+        Author author = authorDAO.findOne(id);
+        if (author == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(author.getName()).build();
+    }
 
     @Path("/create")
     @POST
